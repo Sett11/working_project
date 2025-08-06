ea8bcb8743b66f80ef8fea80d6a90d43ea0d7c76
Jhonny <gonzodzen@mail.ru>
2025-08-07 00:05:33 +0300

    feat: The list of air conditioner brands has been expanded and the upper price threshold has been increased
    - New brands of air conditioners have been added: "Hisense", "Mitsubishi", "Samsung" and "TCL".
    - The upper price threshold for air conditioners has been increased to 15,000 BYN to improve the selection.

--- new_project/form_com_offer/front/front.py
+++ new_project/form_com_offer/front/front.py
@@ -452,8 +452,8 @@ with gr.Blocks(title="Автоматизация продаж кондицион
                     installation_price = gr.Number(label="Стоимость монтажа (BYN)", minimum=0, step=1, value=get_placeholder_order()["order_params"]["installation_price"])
             gr.Markdown("### 3. Требования к кондиционеру")
             with gr.Row():
-                brand = gr.Dropdown(["Любой", "Midea", "Dantex", "Vetero", "Electrolux", "Toshiba"], label="Бренд", value=get_placeholder_order()["aircon_params"]["brand"])
-                price = gr.Slider(0, 10000, value=get_placeholder_order()["aircon_params"]["price_limit"], label="Верхний порог стоимости (BYN)")
+                brand = gr.Dropdown(["Любой", "Midea", "Dantex", "Vetero", "Electrolux", "Toshiba", "Hisense", "Mitsubishi", "Samsung", "TCL"], label="Бренд", value=get_placeholder_order()["aircon_params"]["brand"])
+                price = gr.Slider(0, 15000, value=get_placeholder_order()["aircon_params"]["price_limit"], label="Верхний порог стоимости (BYN)")
                 inverter = gr.Checkbox(label="Инверторный компрессор", value=get_placeholder_order()["aircon_params"]["inverter"])
                 wifi = gr.Checkbox(label="Wi-Fi управление", value=get_placeholder_order()["aircon_params"]["wifi"])
             with gr.Row():
